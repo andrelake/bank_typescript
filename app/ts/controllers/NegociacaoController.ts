9186c448e3264230f9c2799e367324943ac4e605
andrelake <andre.lake@hotmail.com>
2020-06-27 22:20:53 -0300

    NegociacaoController

---
+++ app/ts/controllers/NegociacaoController.ts
@@ -0,0 +1,25 @@
+class NegociacaoController {
+
+    private _inputData;
+    private _inputQuantidade;
+    private _inputValor;
+
+    constructor() {
+
+        this._inputData = document.querySelector('#data')
+        this._inputQuantidade = document.querySelector('#quantidade')
+        this._inputValor = document.querySelector('#valor')
+    }
+
+    adiciona(event) {
+
+        event.preventDefault();//não recarrega a página ao clicar no 'submit'
+        const negociacao = new Negociacao(
+            this._inputData.value,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
+
+        console.log(negociacao)
+    }
+}
