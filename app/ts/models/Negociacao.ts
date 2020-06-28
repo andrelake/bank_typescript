22903d267a2be83d0f2b6a24707fe914fdd669dc
andrelake <andre.lake@hotmail.com>
2020-06-27 22:36:09 -0300

    Refactor propriedades no construtor do modelo Negociacao

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,29 +1,25 @@
 class Negociacao {
 
-    private _data;
-    private _quantidade;
-    private _valor;
-
-    constructor(data, quantidade, valor) {
-
-        this._data = data   // "_" indica que os valores não podem ser alterados fora dos métodos da própria classe
-        this._quantidade = quantidade
-        this._valor = valor
-    }
+    constructor(private _data: Date, private _quantidade: number, private _valor: number) {
+   }
 
     get data() {
+        
         return this._data
     }
 
     get quantidade() {
+        
         return this._quantidade
     }
 
     get valor() {
+        
         return this._valor
     }
 
     get volume() {
+        
         return this._quantidade * this._valor
     }
 }
